5a85334fdf813010f180c80cd2adcb818c7f5b0c
Winfidonarleyan <dowlandtop@yandex.com>
2022-06-11 13:36:32 +0700

    feat(Common/Crypto): add basic support OpenSSl 3.0

--- src/common/Cryptography/CryptoConstants.h
+++ src/common/Cryptography/CryptoConstants.h
@@ -15,8 +15,8 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef AZEROTHCORE_CRYPTO_CONSTANTS_H
-#define AZEROTHCORE_CRYPTO_CONSTANTS_H
+#ifndef WARHEAD_CRYPTO_CONSTANTS_H
+#define WARHEAD_CRYPTO_CONSTANTS_H
 
 #include "Define.h"
 
@@ -24,7 +24,8 @@ namespace Warhead::Crypto
 {
     struct Constants
     {
-        static constexpr size_t SHA1_DIGEST_LENGTH_BYTES   = 20;
+        static constexpr size_t MD5_DIGEST_LENGTH_BYTES = 16;
+        static constexpr size_t SHA1_DIGEST_LENGTH_BYTES = 20;
         static constexpr size_t SHA256_DIGEST_LENGTH_BYTES = 32;
     };
 }
--- src/common/Cryptography/CryptoHash.h
+++ src/common/Cryptography/CryptoHash.h
@@ -35,10 +35,10 @@ namespace Warhead::Impl
         typedef EVP_MD const* (*HashCreator)();
 
 #if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L
-        static EVP_MD_CTX* MakeCTX() { return EVP_MD_CTX_create(); }
+        static EVP_MD_CTX* MakeCTX() noexcept { return EVP_MD_CTX_create(); }
         static void DestroyCTX(EVP_MD_CTX* ctx) { EVP_MD_CTX_destroy(ctx); }
 #else
-        static EVP_MD_CTX* MakeCTX() { return EVP_MD_CTX_new(); }
+        static EVP_MD_CTX* MakeCTX() noexcept { return EVP_MD_CTX_new(); }
         static void DestroyCTX(EVP_MD_CTX* ctx) { EVP_MD_CTX_free(ctx); }
 #endif
     };
@@ -59,7 +59,7 @@ namespace Warhead::Impl
         }
 
         template <typename... Ts>
-        static auto GetDigestOf(Ts&& ... pack) -> std::enable_if_t < !(std::is_integral_v<std::decay_t<Ts>> || ...), Digest >
+        static auto GetDigestOf(Ts&&... pack) -> std::enable_if_t<std::conjunction_v<std::negation<std::is_integral<Ts>>...>, Digest>
         {
             GenericHash hash;
             (hash.UpdateData(std::forward<Ts>(pack)), ...);
@@ -73,15 +73,43 @@ namespace Warhead::Impl
             ASSERT(result == 1);
         }
 
+        GenericHash(GenericHash const& right) : _ctx(GenericHashImpl::MakeCTX())
+        {
+            *this = right;
+        }
+
+        GenericHash(GenericHash&& right) noexcept
+        {
+            *this = std::move(right);
+        }
+
         ~GenericHash()
         {
             if (!_ctx)
-            {
                 return;
-            }
-
             GenericHashImpl::DestroyCTX(_ctx);
             _ctx = nullptr;
+        }
+
+        GenericHash& operator=(GenericHash const& right)
+        {
+            if (this == &right)
+                return *this;
+
+            int result = EVP_MD_CTX_copy_ex(_ctx, right._ctx);
+            ASSERT(result == 1);
+            _digest = right._digest;
+            return *this;
+        }
+
+        GenericHash& operator=(GenericHash&& right) noexcept
+        {
+            if (this == &right)
+                return *this;
+
+            _ctx = std::exchange(right._ctx, GenericHashImpl::MakeCTX());
+            _digest = std::exchange(right._digest, Digest{});
+            return *this;
         }
 
         void UpdateData(uint8 const* data, size_t len)
@@ -89,11 +117,9 @@ namespace Warhead::Impl
             int result = EVP_DigestUpdate(_ctx, data, len);
             ASSERT(result == 1);
         }
-
         void UpdateData(std::string_view str) { UpdateData(reinterpret_cast<uint8 const*>(str.data()), str.size()); }
         void UpdateData(std::string const& str) { UpdateData(std::string_view(str)); } /* explicit overload to avoid using the container template */
         void UpdateData(char const* str) { UpdateData(std::string_view(str)); } /* explicit overload to avoid using the container template */
-
         template <typename Container>
         void UpdateData(Container const& c) { UpdateData(std::data(c), std::size(c)); }
 
@@ -103,8 +129,6 @@ namespace Warhead::Impl
             int result = EVP_DigestFinal_ex(_ctx, _digest.data(), &length);
             ASSERT(result == 1);
             ASSERT(length == DIGEST_LENGTH);
-            GenericHashImpl::DestroyCTX(_ctx);
-            _ctx = nullptr;
         }
 
         Digest const& GetDigest() const { return _digest; }
@@ -117,10 +141,9 @@ namespace Warhead::Impl
 
 namespace Warhead::Crypto
 {
+    using MD5 = Warhead::Impl::GenericHash<EVP_md5, Constants::MD5_DIGEST_LENGTH_BYTES>;
     using SHA1 = Warhead::Impl::GenericHash<EVP_sha1, Constants::SHA1_DIGEST_LENGTH_BYTES>;
     using SHA256 = Warhead::Impl::GenericHash<EVP_sha256, Constants::SHA256_DIGEST_LENGTH_BYTES>;
-
-    WH_COMMON_API std::string GetMD5HashFromFile(std::string const& filePath);
 }
 
 #endif
