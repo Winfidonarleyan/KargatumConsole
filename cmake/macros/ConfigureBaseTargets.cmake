cd07eb563c4a13848a9d2fa5f6f5d826d821111e
Winfidonarleyan <dowlandtop@yandex.com>
2022-07-01 08:33:30 +0700

    feat(Misc): start support c++20

--- cmake/macros/ConfigureBaseTargets.cmake
+++ cmake/macros/ConfigureBaseTargets.cmake
@@ -7,6 +7,7 @@
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
 
 # An interface library to make the target com available to other targets
 add_library(warhead-compile-option-interface INTERFACE)
@@ -14,25 +15,9 @@ add_library(warhead-compile-option-interface INTERFACE)
 # Use -std=c++11 instead of -std=gnu++11
 set(CXX_EXTENSIONS OFF)
 
-# An interface library to make the target features available to other targets
-add_library(warhead-feature-interface INTERFACE)
-
-target_compile_features(warhead-feature-interface
-  INTERFACE
-    cxx_std_17
-    cxx_alias_templates
-    cxx_auto_type
-    cxx_constexpr
-    cxx_decltype
-    cxx_decltype_auto
-    cxx_final
-    cxx_lambdas
-    cxx_generic_lambdas
-    cxx_variadic_templates
-    cxx_defaulted_functions
-    cxx_nullptr
-    cxx_trailing_return_types
-    cxx_return_type_deduction)
+# Enable C++20 support
+set(CMAKE_CXX_STANDARD 20)
+message(STATUS "Enabled С++20 standard")
 
 # An interface library to make the warnings level available to other targets
 # This interface taget is set-up through the platform specific script
@@ -40,15 +25,15 @@ add_library(warhead-warning-interface INTERFACE)
 
 # An interface used for all other interfaces
 add_library(warhead-default-interface INTERFACE)
+
 target_link_libraries(warhead-default-interface
   INTERFACE
-    warhead-compile-option-interface
-    warhead-feature-interface)
+    warhead-compile-option-interface)
 
 # An interface used for silencing all warnings
 add_library(warhead-no-warning-interface INTERFACE)
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+if (MSVC)
   target_compile_options(warhead-no-warning-interface
     INTERFACE
       /W0)
